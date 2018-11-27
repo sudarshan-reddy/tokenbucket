df690e74895889971ad601a008f2305382c35efb
Sudarshan <sudar.theone@gmail.com>
2018-11-27 12:39:00 +0530

    bucket: minor readability improvements

--- bucket.go
+++ bucket.go
@@ -12,10 +12,10 @@ const infinityDuration time.Duration = 0x7fffffffffffffff
 type Bucket struct {
 	sync.Mutex
 
-	startTime    time.Time
-	capacity     int64
-	quantum      int64
-	fillInterval time.Duration
+	startTime       time.Time
+	capacity        int64
+	tokensToBeAdded int64
+	fillInterval    time.Duration
 
 	availableTokens int64
 	latestTick      int64
@@ -28,7 +28,7 @@ func NewBucket(fillInterval time.Duration, capacity int64) *Bucket {
 		latestTick:      0,
 		fillInterval:    fillInterval,
 		capacity:        capacity,
-		quantum:         1,
+		tokensToBeAdded: 1,
 		availableTokens: capacity,
 	}
 }
@@ -83,7 +83,7 @@ func (tb *Bucket) Capacity() int64 {
 
 // Rate returns the fill rate of the bucket, in tokens per second.
 func (tb *Bucket) Rate() float64 {
-	return 1e9 * float64(tb.quantum) / float64(tb.fillInterval)
+	return 1e9 * float64(tb.tokensToBeAdded) / float64(tb.fillInterval)
 }
 
 func (tb *Bucket) take(now time.Time, count int64, maxWait time.Duration) (time.Duration, bool) {
@@ -92,22 +92,21 @@ func (tb *Bucket) take(now time.Time, count int64, maxWait time.Duration) (time.
 	}
 
 	tick := tb.currentTick(now)
-	tb.adjustavailableTokens(tick)
-	avail := tb.availableTokens - count
-	if avail >= 0 {
-		tb.availableTokens = avail
+	tb.adjustAvailableTokens(tick)
+	availableTokens := tb.availableTokens - count
+	if availableTokens >= 0 {
+		tb.availableTokens = availableTokens
 		return 0, true
 	}
 
-	// endTick holds the tick when all the requested tokens will
-	// become available.
-	endTick := tick + (-avail+tb.quantum-1)/tb.quantum
+	//calculate how long to wait for the tokens to be available.
+	endTick := tick + (-availableTokens+tb.tokensToBeAdded-1)/tb.tokensToBeAdded
 	endTime := tb.startTime.Add(time.Duration(endTick) * tb.fillInterval)
 	waitTime := endTime.Sub(now)
 	if waitTime > maxWait {
 		return 0, false
 	}
-	tb.availableTokens = avail
+	tb.availableTokens = availableTokens
 	return waitTime, true
 }
 
@@ -115,11 +114,11 @@ func (tb *Bucket) currentTick(now time.Time) int64 {
 	return int64(now.Sub(tb.startTime) / tb.fillInterval)
 }
 
-func (tb *Bucket) adjustavailableTokens(tick int64) {
+func (tb *Bucket) adjustAvailableTokens(tick int64) {
 	if tb.availableTokens >= tb.capacity {
 		return
 	}
-	tb.availableTokens += (tick - tb.latestTick) * tb.quantum
+	tb.availableTokens += (tick - tb.latestTick) * tb.tokensToBeAdded
 	if tb.availableTokens > tb.capacity {
 		tb.availableTokens = tb.capacity
 	}
